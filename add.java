1e2d3868dda0662c46fa4dfab4050243afd992e5
SIVASHALINI.G <112835470+SivashaliniG18@users.noreply.github.com>
2022-09-07 14:46:21 +0530

    Addition of two Numbers

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class demo{
+    public static void main(String a[])
+    {
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter the values:");
+        int n1=in.nextInt();
+        int n2=in.nextInt();
+        int c=n1+n2;
+        System.out.print("The addition of two number is :"+c);
+    }
+}
