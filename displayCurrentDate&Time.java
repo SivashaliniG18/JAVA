6ed7834cb93d54da4ec5175e9c16825b746ffd4f
SIVASHALINI.G <112835470+SivashaliniG18@users.noreply.github.com>
2022-09-20 19:19:18 +0530

    To display the current Date and Time

---
+++ displayCurrentDate&Time.java
@@ -0,0 +1,10 @@
+import java.time.*;
+
+public class demo {
+
+    public static void main(String[] args) {
+        LocalDateTime dt = LocalDateTime.now();
+
+        System.out.println("Current Date and Time is: " + dt);
+    }
+}
