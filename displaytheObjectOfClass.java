108cfa59195f212ed22b5dd6c05eabd6d604992f
SIVASHALINI.G <112835470+SivashaliniG18@users.noreply.github.com>
2022-09-22 18:46:42 +0530

    Display the object of a class

---
+++ displaytheObjectOfClass.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class sample{
+    
+}
+class demo {
+    public static void main(String[] args) {
+        sample s=new sample();
+        System.out.println("The object for sample class is : "+s);
+    }
+}
