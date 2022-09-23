348026546b4e9bf3f9d4ca29d43ccda9c636bdfa
SIVASHALINI.G <112835470+SivashaliniG18@users.noreply.github.com>
2022-09-23 18:52:53 +0530

    Convert the given character to string

---
+++ covertCharactertoString.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class demo{
+
+  public static void main(String[] args)throws Exception
+  {
+   
+    
+    char s='d';
+    System.out.print("Enter the string with some white space: "+s+"\n");
+     String st = Character.toString(s);
+   
+        System.out.println("After replacement: " + st);
+  }
+}
