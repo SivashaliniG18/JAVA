b089fb8dda0cbc61d8104f613defb0d2f8d45964
SIVASHALINI.G <112835470+SivashaliniG18@users.noreply.github.com>
2022-09-29 19:37:48 +0530

    Display the current working Directory path

---
+++ displayCurrentDirectory.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class demo {
+
+  public static void main(String[] args)throws Exception
+  {
+    
+        String path = System.getProperty("user.dir");
+        
+        System.out.println("Working Directory = " + path);
+  }
+}
