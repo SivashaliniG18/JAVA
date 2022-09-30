eea7cd436928842e54503f5e862f9f64af5ea1fd
SIVASHALINI.G <112835470+SivashaliniG18@users.noreply.github.com>
2022-09-30 07:36:31 +0530

    convert the given boolean value to string

---
+++ booleanToString.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class demo {
+  public static void main(String[] args)throws Exception
+  {
+    boolean Value1 = true;
+    boolean Value2 = false;
+    String stringValue1 = String.valueOf(booleanValue1);
+    String stringValue2 = String.valueOf(booleanValue2);
+    System.out.println(stringValue1);  
+    System.out.println(stringValue2);    
+  }
+}
