944ab9de07d099474209ba5d344145a6e50ac53a
SIVASHALINI.G <112835470+SivashaliniG18@users.noreply.github.com>
2022-09-30 07:43:40 +0530

    Create stringToInteger.java

---
+++ stringToInteger.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class demo {
+  public static void main(String[] args)throws Exception
+  {
+     String s1 = "567879";
+    String s2 = "08465365";
+    int num1 = Integer.parseInt(s1);
+    int num2 = Integer.parseInt(s2);
+    System.out.println(num1);
+    System.out.println(num2);     
+  }
+}
