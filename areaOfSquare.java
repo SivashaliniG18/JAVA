63c6a3840fe3872a031b816485515940de82e038
SIVASHALINI.G <112835470+SivashaliniG18@users.noreply.github.com>
2022-10-03 18:47:56 +0530

    find the area & peremeter of square

--- areaOfSquare.java
+++ areaOfSquare.java
@@ -3,11 +3,11 @@ class demo{
     public static void main(String[] args) throws Exception
     {
         DataInputStream d=new DataInputStream(System.in);
-        System.out.println("Enter the radius: ");
+        System.out.println("Enter the value : ");
         long r=Long.parseLong(d.readLine());
-        double peremeter=2*3.14*r;
-        double area=3.14*r*r;
-        System.out.print("Area of circle is:"+area);
-        System.out.print("Premeter of circle is:"+premeter);
+        double peremeter=4*r;
+        double area=r*r;
+        System.out.print("Area of Square is:"+area);
+        System.out.print("Premeter of Square is:"+premeter);
     }
 }
